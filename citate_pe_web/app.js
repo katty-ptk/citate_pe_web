04a8d69768a62c3004c0825c580230fe3c68db6e
Katty <46168818+katty1302@users.noreply.github.com>
2020-07-01 07:53:06 +0300

    adding folder from computer

---
+++ citate_pe_web/app.js
@@ -0,0 +1,87 @@
+// document.body.style.backgroundColor =
+//   "background: radial-gradient(circle, rgba(174,238,181,1) 0%, rgba(233,148,148,1) 100%)"; // background color
+
+var quotes = [
+  "You are amazing. ❤",
+  "Viata este asa cum o facem noi.",
+  "Negativity needs to be be mastered.",
+  "YOU ARE ENOUGH! ❤",
+  "You rock! ❤",
+  "Ana are intotdeauna dreptate.",
+  "It's not happiness that makes us grateful. It's gratefulness that makes us happy.",
+  "Without a struggle, there can be no progress.",
+  "You are imperfect, permanently and inevitably flawed. And you are beautiful.",
+  "You can't change other people, but you can change yourself.",
+  "Happiness is not found outside of us. It's found within.",
+  "Those who know they have enough are truly wealthy.",
+  "We sat in silence, letting the green in the air heal what it could.",
+  "Life is a series of building, testing, changing and iterating.",
+  "The inspiration you seek is already within you. Be silent and listen.",
+  "Things work out best for those who make the best of how things work out.",
+  "I survived because the fire within me was stronger than the fire around me.",
+  "May your choices reflect your hopes, not your fears.",
+  "'Orice viaţă este un miracol.' -> Micul Print",
+  "Sunt singur… sunt singur… sunt singur… – răspunse ecoul.",
+  "Whatever you think your identity is, you will operate accordinly.",
+  "Dacă cineva are propriul 'de ce' al vieții, se împacă aproape cu orice 'cum'.",
+  "Poate ca fericirea se găsește întotdeauna in călătoria ascendenta, nu în senzația trecătoare.",
+  "Impossible is just a big word thrown around by small men and women who find it easier to live in the world they've been given than to explore the power they have to change it.",
+  "See: it's a never-ending upward spiral. And if you think at any point you're allowed to stop climbing, I'm afraid you're missing the point. Because the joy is in the climb itself.",
+  "Was it a bad day? Or was it a five bad minutes that you milked all day?",
+  "Follow your bliss and the universe will open doors where there were only walls.",
+  "The death of a dream is the day that you stop believing in the work it takes to get there.",
+  "Tell your fucking truth to someone – it might just save your life, heal you from deep within, and connect you to humanity in ways you never imagined.",
+  "Compara-te cu cine ai fost tu ieri, nu cu cine este altcineva astazi.",
+  "Poarta-te ca si cand ceea ce faci conteaza. Pentru ca asa si este.",
+  "Cel care muta un munte incepe intotdeauna prin a indrepta pietrele mai mici.",
+  "Nu fa rau si acesta va inceta sa mai existe.",
+  "Be kind! 💕",
+  "We become what we think about most of the time, and that's the strangest secret.",
+]; // the default quotes which will randomly change
+
+var quote_container = document.querySelector("#quote-container"); // grab the div in which the main quote will be placed
+var quotesBtn = document.querySelector("#quotes-list-btn"); // grab the button which will show all the quotes
+var quotesDiv = document.querySelector("#quotesDiv"); // grab the div in which the quotes will be put
+// quotesLis = document.querySelectorAll("li[class='quotes-class'");
+
+// MAIN QUOTE
+var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; // the "main" quote which will randomly channge
+var quote_p = document.createElement("p"); // creates paragragraph for the random quote
+quote_container.appendChild(quote_p); // appends the paragraph to the div
+quote_p.textContent += randomQuote; // appends the quote to the paragraph
+quote_p.setAttribute("id", "quoteId"); // adds id to main quote
+
+//
+//
+
+// quotesDiv.style.display = "none";
+
+// // ALL QUOTES
+// var quotesUl = document.createElement('ul'); // creates ul element for the quotes which are lis
+// quotesUl.classList.add('quotes-ul'); // adds class to ul
+// quotesDiv.appendChild(quotesUl); // the new ul becomes child of the div
+
+// for (var i = 0; i < quotes.length; i++) {
+//     quotesUl.innerHTML += "<li class='quotes-class'>" + quotes[i] + "</li><br><br>";
+// } // for each element from the quotes array, a new li with the "quotes-class" class is created and added to the created ul
+
+// // quotesLis.visibility = "hidden";
+
+// quotesBtn.addEventListener('click', function quotesAppear(e) { // when the Quotes button is clicked, the quotesAppear() function will be executed
+//     e.preventDefault();
+
+//     if (quotesLis.visibility === "hidden") { // if the lis have the visibility hidden
+//         for (var j = 0; j < quotesLis.length; j++) { // for each element from the quotes array
+//             quotesLis[j].visibility = 'visible'; // the visibility is becoming visible
+//         }
+//         quotesBtn.right = "0px"; // and the button is blocked on the screen
+//         alert('visibility is visible');
+//     } else { // else, so if the lis are visible
+//         for (var k = 0; k < quotesLis.length; k++) { // for each element from the array
+//             quotesLis[k].visibility = 'hidden'; // the visibility will become hidden
+//         }
+
+//         alert('visibility is hidden');
+//     }
+
+// });
